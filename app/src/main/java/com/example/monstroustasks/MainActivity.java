5df47914b9d4110e3e2e364ec009ce6126d596fd
adamvanw <80796919+adamvanw@users.noreply.github.com>
2024-11-07 15:15:27 -0600

    Added activity_profile.xml and two controller classes.

--- app/src/main/java/com/example/monstroustasks/MainActivity.java
+++ app/src/main/java/com/example/monstroustasks/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.monstroustasks;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +19,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button session_button = findViewById(R.id.start_session);
+        Button settings_button = findViewById(R.id.settings);
+        Button profile_button = findViewById(R.id.profile);
+        Button credits_button = findViewById(R.id.credits);
 
+        credits_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, CreditsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        profile_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
