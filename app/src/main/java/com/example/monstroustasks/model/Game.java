8e15997f1b6baab302aa4cd242e54c5150568eeb
adamvanw <80796919+adamvanw@users.noreply.github.com>
2024-11-07 14:54:49 -0600

    Changed and added functionality to Game.java.

--- app/src/main/java/com/example/monstroustasks/model/Game.java
+++ app/src/main/java/com/example/monstroustasks/model/Game.java
@@ -1,26 +1,47 @@
 package com.example.monstroustasks.model;
 
-import android.util.Pair;
-
 import java.util.ArrayList;
 
 public class Game {
-    ArrayList<Pair<Task, Boolean>> tasks;
+    ArrayList<Monster> monsters;
     int health;
-    int sessionID;
+    // totalHealth will be loaded in with loadMonsters() and should not be changed during the game.
+    // Health should be changed with updateHealth() after a Monster has been defeated.
+    int totalHealth;
 
-    public Game(ArrayList<Pair<Task, Boolean>> tasks, int health, int sessionID) {
-        this.tasks = tasks;
-        this.health = health;
-        this.sessionID = sessionID;
+    public Game(ArrayList<Monster> monsters, int totalHealth) {
+        this.monsters = monsters;
+        this.totalHealth = totalHealth;
     }
 
-    public ArrayList<Pair<Task, Boolean>> getTasks() {
-        return this.tasks;
+    public void loadMonsters(ArrayList<Task> tasks) {
+        this.monsters = new ArrayList<Monster>();
+        for (int i = 0; i < tasks.size(); i++) {
+            this.addMonster(new Monster(tasks.get(i)));
+        }
+        this.updateHealth();
+        // total health is set to the initial amount of health.
+        this.setTotalHealth(this.getHealth());
     }
 
-    public void setTasks(ArrayList<Pair<Task, Boolean>> tasks) {
-        this.tasks = tasks;
+    public ArrayList<Monster> getMonsters() {
+        return this.monsters;
+    }
+
+    public void setMonsters(ArrayList<Monster> monsters) {
+        this.monsters = monsters;
+    }
+
+    public void addMonster(Monster monster) {
+        this.getMonsters().add(monster);
+    }
+
+    public int getTotalHealth() {
+        return this.totalHealth;
+    }
+
+    public void setTotalHealth(int health) {
+        this.totalHealth = health;
     }
 
     public int getHealth() {
@@ -31,11 +52,11 @@ public class Game {
         this.health = health;
     }
 
-    public int getSessionID() {
-        return this.sessionID;
-    }
-
-    public void setSessionID(int sessionID) {
-        this.sessionID = sessionID;
+    public void updateHealth() {
+        int health = 0;
+        for (int i = 0; i < this.getMonsters().size(); i++) {
+            health += this.getMonsters().get(i).getDifficulty();
+        }
+        this.setHealth(health);
     }
 }
