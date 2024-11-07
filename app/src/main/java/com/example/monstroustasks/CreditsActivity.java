62c81cb64256d99af1bb39d91d533967f32c8466
adamvanw <80796919+adamvanw@users.noreply.github.com>
2024-11-07 15:32:50 -0600

    Changed bottom border name

--- app/src/main/java/com/example/monstroustasks/CreditsActivity.java
+++ app/src/main/java/com/example/monstroustasks/CreditsActivity.java
@@ -1,6 +1,9 @@
 package com.example.monstroustasks;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +18,15 @@ public class CreditsActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+
+        Button home_button = findViewById(R.id.home_button);
+
+        home_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CreditsActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
