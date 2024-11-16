073b038e803561309d3f353447a4a91789ffa724
Blazentesrill <decmpetershack@gmail.com>
2024-11-16 12:12:01 -0600

    Fixed home button for ProfileActivity

--- app/src/main/java/com/example/monstroustasks/ProfileActivity.java
+++ app/src/main/java/com/example/monstroustasks/ProfileActivity.java
@@ -1,6 +1,9 @@
 package com.example.monstroustasks;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +18,15 @@ public class ProfileActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Button home_button = findViewById(R.id.home_button);
+
+        home_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
